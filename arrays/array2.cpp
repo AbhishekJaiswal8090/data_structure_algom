ccf3b23554e45e76f6487f82f1f77211a378bd20
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-04 22:29:32 +0530

    Maximum Subarray sum

--- arrays/array2.cpp
+++ arrays/array2.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void PrintingSubArray()
 {
+
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(int);
     for (int i = 0; i < n; i++)
@@ -17,4 +18,43 @@ int main()
         }
         cout << endl;
     }
+}
+
+
+// Brute Force
+void MaxSubArraySum()
+{
+    int sum=0;
+    int max=0;
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << ",";
+                sum +=arr[k];
+            }
+            cout<<"sum: "<<sum<<endl;
+            if(sum > max){
+                max=sum;
+            }
+            sum=0;
+            cout << endl;
+        }
+        cout << endl;
+    }
+     cout<<"Max val is: "<<max<<endl;
+}
+
+
+
+
+
+int main()
+{
+    // PrintingSubArray();
+    MaxSubArraySum();
 }
