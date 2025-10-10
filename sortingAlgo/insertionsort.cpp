5d38a1e86f461e8c068f4ad481e6f2de9c523f77
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-10 21:11:12 +0530

    fully optimized insertion sort

---
+++ sortingAlgo/insertionsort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+// insertion sort 
+
+
+void printArr(int *arr , int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<",";
+    }
+}
+
+
+
+void InsertionSort(int *arr, int n){
+    for(int i=0; i<n-2; i++){
+        int minIdx=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[minIdx]){
+                 minIdx=j;
+            }
+           
+        }
+        swap(arr[i],arr[minIdx]);
+    }
+
+    printArr(arr,n );
+}
+
+
+int main(){
+  
+    int arr[]={5,7,3,9,0};
+    int n =sizeof(arr)/sizeof(int);
+    InsertionSort(arr,n); 
+
+
+}
