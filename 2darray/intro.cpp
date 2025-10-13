ba36b09a93cd07a4136246e828e61ed107c22116
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-13 13:02:27 +0530

    soln of spiral matrix

--- 2darray/intro.cpp
+++ 2darray/intro.cpp
@@ -1,47 +1,53 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-// 2D ARRAYS 
+// 2D ARRAYS
 
+int main()
+{
+    cout << "<==2D ARRAY-S==>" << endl;
+    float student[2][5] = {{5.9, 5.5, 6, 5.9, 5.11},
+                           {55, 67, 75, 68, 56}};
 
-int main(){
-    cout<<"<==2D ARRAY-S==>"<<endl; 
-    float student [2][5]={{5.9,5.5,6,5.9,5.11},
-                          {55,67,75,68,56}};
-
-    for(int i=0; i<2; i++){
-        for(int j =0; j<5; j++){
-            cout <<student[i][j]<<",";
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << student[i][j] << ",";
         }
     }
 
     // I/O 2d array;
     int arr[3][4];
-    for(int i=0; i<3; i++){
-        for(int j=0; j<4; j++){
-            cin>>arr[i][j];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    for(int i=0; i<3; i++){
-        for(int j=0; j<4; j++){
-            cout<<arr[i][j]<<",";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << ",";
         }
-        cout<<endl;
+        cout << endl;
     }
-    
-   cout<<endl;
+
+    cout << endl;
     // modifying and printing
-    for(int i=0; i<4; i++){
-        for(int j=0; j<3; j++){
-            cout<<arr[j][i]<<",";
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[j][i] << ",";
         }
-        cout<<endl;
-    }    
+        cout << endl;
+    }
 
-    // 2d arrays in memory still are stored as linearly just like 
-    // 1d arrays 
-
-
+    // 2d arrays in memory still are stored as linearly just like
+    // 1d arrays
 }
--- 2darray/spiralmat.cpp
+++ 2darray/spiralmat.cpp
@@ -1,11 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
 // spiral matrix problem;
-int main(){
+void spiralMatrix(int arr[][4], int n, int m)
+{
+    int srow = 0;
+    int erow = n - 1;
+    int scolum = 0;
+    int ecolum = n - 1;
+    while (srow <= erow && scolum <= ecolum)
+    {
+        // top
+        for (int j = scolum; j <= ecolum; j++)
+        {
+            cout << arr[srow][j] << " ";
+        }
+        // right
+        for (int i = srow + 1; i <= erow; i++)
+        {
+            cout << arr[i][ecolum] << " ";
+        }
+        // bottom
+        for (int k = ecolum - 1; k >= scolum; k--)
+        {
+            cout << arr[erow][k] << " ";
+        }
+        // left
+        for (int l = erow - 1; l >= srow + 1; l--)
+        {
+            cout << arr[l][scolum] << " ";
+        }
+        srow++;
+        scolum++;
+        erow--;
+        ecolum--;
+    }
+    cout << endl;
+}
+int main()
+{
 
-    
+    int matrix[4][4] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
 
-
+    spiralMatrix(matrix, 4, 4);
 }
