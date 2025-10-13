626d6e535933e58e541a53f69499645a7738ef9e
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-13 11:41:14 +0530

    intro to 2d array

---
+++ 2darray/intro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+// 2D ARRAYS 
+
+
+int main(){
+    cout<<"<==2D ARRAY-S==>";
+    int student [5][5]={{5.9,5.5,6,5.9,5.11},{55,67,75,68,56}};
+    
+
+}
