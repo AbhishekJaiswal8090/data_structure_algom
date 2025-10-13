e44b577c7cc0485573a25bcce44aa71c89d0bbb5
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-13 12:01:40 +0530

    modifying aray index

--- 2darray/intro.cpp
+++ 2darray/intro.cpp
@@ -30,6 +30,15 @@ int main(){
         }
         cout<<endl;
     }
+    
+   cout<<endl;
+    // modifying and printing
+    for(int i=0; i<4; i++){
+        for(int j=0; j<3; j++){
+            cout<<arr[j][i]<<",";
+        }
+        cout<<endl;
+    }    
 
 
 }
