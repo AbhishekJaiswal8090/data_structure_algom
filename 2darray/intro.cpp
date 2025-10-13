2ba01bb6017ff3703604b6c3a98396a7118974c2
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-13 11:50:27 +0530

    2d arrays

--- 2darray/intro.cpp
+++ 2darray/intro.cpp
@@ -5,8 +5,16 @@ using namespace std;
 
 
 int main(){
-    cout<<"<==2D ARRAY-S==>";
-    int student [5][5]={{5.9,5.5,6,5.9,5.11},{55,67,75,68,56}};
-    
+    cout<<"<==2D ARRAY-S==>"<<endl; 
+    float student [2][5]={{5.9,5.5,6,5.9,5.11},
+                          {55,67,75,68,56}};
+
+    for(int i=0; i<2; i++){
+        for(int j =0; j<5; j++){
+            cout <<student[i][j]<<",";
+        }
+    }
+
+
 
 }
