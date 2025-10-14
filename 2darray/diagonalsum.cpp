c2bc0969d15c29c9953916a5d8df3cab40fab5a0
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-14 22:34:35 +0530

    searching in sorted matrix

--- 2darray/diagonalsum.cpp
+++ 2darray/diagonalsum.cpp
@@ -1,18 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // diagonal sum problem
-int diagonalSum(int mat[3][3] ,int n){
-    int sum =0;
-    for(int i=0; i<n; i++){
+int diagonalSum(int mat[3][3], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
         sum += mat[i][i];
-        sum +=mat[i][n-i-1];
+        sum += mat[i][n - i - 1];
     }
-    if(n%2==1){
-        sum -= mat[n/2][n/2];
+    if (n % 2 == 1)
+    {
+        sum -= mat[n / 2][n / 2];
     }
     return sum;
 }
-int main(){
-
+int main()
+{
+    int mat[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    cout << diagonalSum(mat, 3);
 }
