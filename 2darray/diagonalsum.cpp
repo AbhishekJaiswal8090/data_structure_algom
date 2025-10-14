3f62ef53010749fb32356ca54e9a73bf7b211d70
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-14 22:31:18 +0530

    fully optimized digonalsum

--- 2darray/diagonalsum.cpp
+++ 2darray/diagonalsum.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 // diagonal sum problem
+int diagonalSum(int mat[3][3] ,int n){
+    int sum =0;
+    for(int i=0; i<n; i++){
+        sum += mat[i][i];
+        sum +=mat[i][n-i-1];
+    }
+    if(n%2==1){
+        sum -= mat[n/2][n/2];
+    }
+    return sum;
+}
 int main(){
 
 }
