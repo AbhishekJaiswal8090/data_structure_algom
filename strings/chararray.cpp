e5621047c9a0357c10af0150f7751f2ce87a951a
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-15 20:07:16 +0530

    input and output string and chars

--- strings/chararray.cpp
+++ strings/chararray.cpp
@@ -16,11 +16,25 @@ int main()
 
     char str1[3]={'c','d','\0'};
     cout<<str1<<endl;
+
+     char work[]="code"; //string literal
+    cout<<work<<endl;
    
     // Input/output
-    char work[]="code"; //string literal
-    cout<<work<<endl;
+    char word[10];
+    cin>>word;
 
-    
+    cout<<"you word was "<<word<<endl;
+
+    // input for whole paragraph
+    // initially char array ignores the word after space character thats why we need
+    // c.getline
+
+    char sentence[50];
+    cin.getline(sentence,50);
+
+    cout<<"your sentence was"<<endl;
+
+
     
 }
