5f077bd2b8d4e7b50ddce59ec88729432e13c5df
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-15 19:56:04 +0530

    char array more detail

---
+++ strings/chararray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// character arrays
+
+int main()
+{
+
+    char str[5] = {'a', 'b', 'c', 'd', 'e'};
+    cout << str[0] << endl;
+    cout << str[1] << endl;
+    cout << str[2] << endl;
+    cout << str[3] << endl;
+    cout << str[4] << endl;
+    
+
+    char str1[3]={'c','d','\0'};
+    cout<<str1<<endl;
+}
--- strings/intro.cpp
+++ strings/intro.cpp
@@ -1,19 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-// basics of chars and strings 
-int main(){
-    char ch1='a';
-    char ch2 ='b';
-    char ch3= 'X';
+// basics of chars and strings
+int main()
+{
+    char ch1 = 'a';
+    char ch2 = 'b';
+    char ch3 = 'X';
     char ch4 = '3';
     char ch5 = '\n';
 
     // ascii val
-   cout<<(int)ch2<<endl;
+    cout << (int)ch2 << endl;
 
-//    to find out the position of any particulaar alphbet
-  int pos= ch3 -'A';
-  cout<<pos<<endl;
+    //    to find out the position of any particulaar alphbet
+    int pos = ch3 - 'A';
+    cout << pos << endl;
+
+    
     return 0;
 }
