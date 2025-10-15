68c74d22f503069b376691a8173a7c0cfb186c8a
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-15 19:43:17 +0530

    basic of char

---
+++ strings/intro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// basics of chars and strings 
+int main(){
+    char ch1='a';
+    char ch2 ='b';
+    char ch3= 'X';
+    char ch4 = '3';
+    char ch5 = '\n';
+
+
+}
