bf1d42edb28255ee36cac822ce6107af5f02b0c7
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-15 19:46:34 +0530

    char in array form

--- strings/intro.cpp
+++ strings/intro.cpp
@@ -9,5 +9,11 @@ int main(){
     char ch4 = '3';
     char ch5 = '\n';
 
+    // ascii val
+   cout<<(int)ch2<<endl;
 
+//    to find out the position of any particulaar alphbet
+  int pos= ch3 -'A';
+  cout<<pos<<endl;
+    return 0;
 }
