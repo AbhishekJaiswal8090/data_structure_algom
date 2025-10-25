5f55362242255b0ecb5c9cf6934dfcf24842df69
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-25 11:50:24 +0530

    vectors in cpp

---
+++ vectors/1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // static array alloaction
+    int arr[10]={3,4,5,6,7,8,9};
+
+    // while vectors ra widely used for dynamic memory and elements alloaction
+    // even space can be shrink and maximize based on the elements we pushed thhrough it
+    
+}
