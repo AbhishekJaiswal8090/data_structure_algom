1780956cd4a297b199a4703ebe109f877ad0e9ca
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-25 13:43:25 +0530

    suffix sum

--- arrays/array4.cpp
+++ arrays/array4.cpp
@@ -1,22 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void PrefixSum(int *arr,int n){
-    int ans[10]={0};
-    int sum=0;
-    for(int i=0; i<n; i++) {
-      sum=sum+arr[i];
-      ans[i]=sum;
+void PrefixSum(int *arr, int n)
+{
+    int ans[10] = {0};
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+        ans[i] = sum;
     }
 
-    for(int i=0; i<n; i++){
-        cout<<ans[i]<<",";
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << ",";
     }
-
-
 }
-int main(){
 
-    int arr[10]={2,4,6,8,10,12,14,16,18,20};
-    PrefixSum(arr,10);
+void SuffixSum(int *arr, int n)
+{
+    int ans2[10] = {0};
+    int sum = 0;
+    for (int j = n - 1; j >= 0; j++)
+    {
+        sum = sum + arr[j];
+        ans2[j] = sum;
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << ans2[j] << ",";
+    }
+}
+int main()
+{
+
+    int arr[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    PrefixSum(arr, 10);
+    cout << "Hello mfs" << endl;
+    SuffixSum(arr, 10);
 }
