e38da276d86ee87a1518dea1c5aa52efea781a1c
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-26 19:44:05 +0530

    MEMORY ALLOCATION AND MEMORY LEAK

--- vectors/2.cpp
+++ vectors/2.cpp
@@ -1,8 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int arr[10]={2,4,6,8,10,12,14,16,18,20};
-    PrefixSum(arr,10);
+    int arr[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    // dynamic memory alocation
+    // allocation at run time
+    // uses heap memory
+
+    // static memory alloaction
+    // allocation at compile time
+    // use stack memory
+
+    // memory leak
+    
 }
