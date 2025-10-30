cacc8de77917011ddb8598392cb3b030668d50ca
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-30 23:11:02 +0530

    bit manipulation basics

--- bitManip/basic.cpp
+++ bitManip/basic.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main(){
     // bit manipulation in cpp
-    
+    // bitwise AND & => 0 & 1 => False (0)
+    // 1 & 0 => False (0)
+    cout<<(3 & 5)<<endl; //results in (1)
+    cout<<(5 & 5)<<endl;
+
+    // bitwise OR  || => anyone is true evrything is true 
+    // false if both values are false 
+    // same goes with numbers if the one is 1 otthers answer is one (1)
+      // and if both are zeroes then the value becomes zero (0) \
+      
+      cout<<(3 || 5)<<endl;
+      cout<<(5 & 5)<<endl;
+
+      
+    return 0;
 }
