0b1d1b03f06e0a7a1a8ca9fc3cbdeaedb431ce18
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-31 20:33:01 +0530

    getting ith position

--- bitManip/practice.cpp
+++ bitManip/practice.cpp
@@ -1,33 +1,54 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // PRACTICE QUESTIONS
-void oddOrEven(int n){
-    if(!(n&1)){
-        cout<<"Even number"<<endl;
-    }else{
-        cout<<"odd number"<<endl;
+void oddOrEven(int n)
+{
+    if (!(n & 1))
+    {
+        cout << "Even number" << endl;
+    }
+    else
+    {
+        cout << "odd number" << endl;
     }
 }
 
-int main(){
+int getPos(int n, int i)
+{
+    int bitmask = (1 << i);
+    if (!(n & bitmask))
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main()
+{
 
     // PREDICT THE OUTPUT FOR ~4
-    
+
     // BIN FOR -> 00000100 NEAGTING IT BECOMES 11111011 AND THEN
     // 1'S COMP BECOMES 0000100 +1  == 0000101 AND THE MSB DETREMINES NUM IS NEGATIVE
-    // SO THE ANSWER WILL BE -5 
-    cout<<(~4)<<endl;
+    // SO THE ANSWER WILL BE -5
+    cout << (~4) << endl;
 
     // PREDICT THE OUTPUT FOR 8 >> 1
-    // 00001000 >> `1 BECOMES => 00000100 
-    cout<<(8>>1)<<endl;
+    // 00001000 >> `1 BECOMES => 00000100
+    cout << (8 >> 1) << endl;
 
     // CEHCK IF NUMS IS ODD OR EVEN
     // USING BIT MANIPULATION
-    oddOrEven(67); //odd
+    oddOrEven(67); // odd
     oddOrEven(8);
 
+    // GET iTH BIT
+    // 1<<i and then take and with bitmask and num
+    cout<<endl<<getPos(7,1);
 
     return 0;
 }
