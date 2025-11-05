7feb4b63a4149bfe8c0810b6e6f6003ae8b71370
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-05 20:02:18 +0530

    experimenting recursion

--- recursion/basic.cpp
+++ recursion/basic.cpp
@@ -1,15 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // recursion
 // when a function repeatedly call itself
 
-int main(){
+// a function with no base case
+void func()
+{
+    cout << "function call works";
+    func();
+}
 
-    
+void printArr(int arr[], int n, int i)
+{
+
+    cout << arr[i] << ",";
+    return printArr(arr, 5, i++);
+}
 
 
+int main()
+{
 
+    // func();
+    int i = 0;
+    int arr[5] = {2, 4, 6, 8, 10};
+    int n = 5;
+    printArr(arr, 5, i);
 
     return 0;
 }
