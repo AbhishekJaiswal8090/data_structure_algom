a3f23d6aacec9d51634f713f3f30023ee7154159
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-05 23:06:35 +0530

    recursion on array

--- recursion/arrayrec.cpp
+++ recursion/arrayrec.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 using namespace std;
 
+// checking if an array is sorted
+// using recursion
+
+
+bool isSorted(int arr[] , int n , int i){
+    if(i==n-1){
+        return true;
+    }
+    if(arr[i] > arr[i+1]){
+        return false;
+    }
+    
+    return isSorted(arr, n, i++);
+};
+
 int main(){
 
-    
+    int arr[5]={2,4,6,8,10};
+   cout<< isSorted(arr , 5, 0);
+   int arr1[5]={0,6,2,5,7};
+   cout<<isSorted(arr1,5,0);
+
 
     return 0;
 }
