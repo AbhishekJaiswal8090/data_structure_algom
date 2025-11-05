98bb0a2ada7f5a6642590545a30a17bd72188c64
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-05 20:15:13 +0530

    factorial with recursion

--- recursion/basic.cpp
+++ recursion/basic.cpp
@@ -5,28 +5,38 @@ using namespace std;
 // when a function repeatedly call itself
 
 // a function with no base case
-void func()
-{
-    cout << "function call works";
-    func();
+// void func()
+// {
+//     cout << "function call works";
+//     func();
+// }
+
+// void printArr(int arr[], int n, int i)
+// {
+
+//     cout << arr[i] << ",";
+//     return printArr(arr, 5, i++);
+// }
+
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    return n* factorial(n-1);
 }
-
-void printArr(int arr[], int n, int i)
-{
-
-    cout << arr[i] << ",";
-    return printArr(arr, 5, i++);
-}
-
 
 int main()
 {
 
-    // func();
-    int i = 0;
-    int arr[5] = {2, 4, 6, 8, 10};
-    int n = 5;
-    printArr(arr, 5, i);
+    // // func();
+    // int i = 0;
+    // int arr[5] = {2, 4, 6, 8, 10};
+    // int n = 5;
+    // printArr(arr, 5, i);
+    int ans = factorial(5);
+    cout<<ans<<endl;
+ 
+
 
     return 0;
 }
