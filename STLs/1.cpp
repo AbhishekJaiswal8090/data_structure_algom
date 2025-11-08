d6a4f5201bcaabe086905581c75e3f2382224ae0
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-08 18:03:23 +0530

    going through cpp stls

--- STLs/1.cpp
+++ STLs/1.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print()
+{
+    cout << "abhishek";
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
-    cout << "Hello, WSL!" << endl;
+    int ans = sum(6, 7);
+    cout << ans;
     return 0;
 }
