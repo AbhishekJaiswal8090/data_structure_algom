592477afa3de9e65b7be95d23858952e32c209a4
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-20 18:03:57 +0530

    sieve algom

--- extrafunc/sieve.cpp
+++ extrafunc/sieve.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-using namespaces std;
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 // Sieve of eratosthanes
 // this algom is widely used for printitng prime number upto n;
@@ -8,6 +11,38 @@ using namespaces std;
 // we use an list size of n
 // heres the complete process
 
+vector<bool> sieve(int n)
+{
+    if (n < 2)
+        return {};
+    vector<bool> arr(n + 1, true);
+    for (int i = 2; i <= n; ++i)
+    {
+        if (arr[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                if (arr[j])
+                {
+                    arr[j] = false;
+                }
+            }
+        }
+    }
+
+    return arr;
+}
+
 int main()
 {
+    int n = 100;
+    vector<bool> primes = sieve(n);
+    for (int i = 2; i <= n; ++i)
+    {
+        if (primes[i])
+            cout << i << ",";
+    }
+
+    cout << '\n';
+    return 0;
 }
