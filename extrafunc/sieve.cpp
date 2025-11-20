62d12c7ad69930380eb48f60d3a37c0acaf91a47
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-20 17:40:55 +0530

    sieve algom

--- extrafunc/sieve.cpp
+++ extrafunc/sieve.cpp
@@ -1,10 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespaces std;
 
 // Sieve of eratosthanes
-// this a
+// this algom is widely used for printitng prime number upto n;
+// instead of visiting every single element and checking if it is prime and printing it
+// we use sieve algom in ordere to reduce time complexity from n^2 to nloglogn
+// we use an list size of n
+// heres the complete process
 
-
-int main(){
-
+int main()
+{
 }
