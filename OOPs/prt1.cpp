b92d875573162061aff0ef235cd597035d316a98
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-24 00:01:15 +0530

    basic of oops

--- OOPs/prt1.cpp
+++ OOPs/prt1.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // in today class we are going to learn all the concepts of ooops and thier implementaation
-// so lets start Object Oriented Programming 
+// so lets start Object Oriented Programming
 // oops is not a pattern but a style of programming
 // it reopresent real world object entities
 
+// often objects have some properties and function 
+// objects are well known as blueprint of classes
+// object are like entities of real world
 
-int main(){
 
-
+int main()
+{
 
 }
