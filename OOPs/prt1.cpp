c13de68f7d78a4daacb19dcfcf5883320c894ece
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-24 00:13:14 +0530

    objects and entities

--- OOPs/prt1.cpp
+++ OOPs/prt1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // in today class we are going to learn all the concepts of ooops and thier implementaation
@@ -10,9 +11,43 @@ using namespace std;
 // objects are well known as blueprint of classes
 // object are like entities of real world
 
-class 
-
-int main()
+class Student
 {
+    // properties
+    string name;
+    int rollno;
+    float sgpa;
+
+    void getPercentage()
+    {
+        cout << (sgpa * 10) << endl;
+    }
+
+};
+
+class User
+{
+    string username;
+    string password;
+    string bio;
+    string newBio;
+
+    void Deactivate()
+    {
+        cout << "Deleting account" << endl;
+    }
+
+    void EditBio()
+    {
+        bio = newBio;
+    }
+};
+
+int main(){
+    User user1;
+    Student std1;
+    // these are nothing but the objects 
+    // in other words these are blueprint of class 
+    // which store some val and perform some task just like real world entities
 
 }
