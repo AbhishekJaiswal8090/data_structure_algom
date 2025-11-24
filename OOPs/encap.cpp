0da58a92a50a3adf07c3b2cbbc326459d1b1da15
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-24 10:06:02 +0530

    encapsulation exmple

--- OOPs/encap.cpp
+++ OOPs/encap.cpp
@@ -4,8 +4,46 @@ using namespace std;
 // Encapsulation
 // => encapsulation is wrapping up data
 // and member functins in a single unit
-// it also implement data hiding
+// it also implement data hiding using acccess modifiers
 
+// lets implement it
+class Bank
+{
+private:
+    int balance;
+
+public:
+    Bank(double initialBalance)
+    {
+        balance = initialBalance;
+    }
+    void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        }
+    }
+
+    void WithDraw(double amount)
+    {
+        if (balance > 0 && amount <= balance)
+        {
+            balance -= amount;
+        }
+    }
+
+    void CurrAmount()
+    {
+        cout << "Your current amount is " << balance << endl;
+    }
+};
 int main()
 {
+    Bank bankAccount(1000);
+    bankAccount.CurrAmount();
+    bankAccount.Deposit(2000);
+    bankAccount.WithDraw(1000);
+    bankAccount.CurrAmount();
+    return 0;
 }
