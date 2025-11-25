fd65a659734cc1dacae219c9923824b09815fddc
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-25 09:26:57 +0530

    deep and shallow copy in cpp

---
+++ OOPs/shalloDeep.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+// here we are going to study about shallow and deep copy
+// Shallow copy => Shalow copy copies references to original array, but array remains same
+
+// Deep copy 
+int main(){
+
+}
