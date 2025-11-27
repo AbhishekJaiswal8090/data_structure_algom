dfafe112e3697d50f6a1f3d785eb0c178ffb15fc
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-27 22:54:24 +0530

    multi level inheritance

--- OOPs/Inheritance/3.cpp
+++ OOPs/Inheritance/3.cpp
@@ -1,9 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // Multi level INheritance
 // Base class = > Derived class  => Derived class
+class Animal
+{
+public:
+    void eat()
+    {
+        cout << "This animal eats food" << endl;
+    }
+};
 
-int main(){
+class Mammal : public Animal
+{
+public:
+    void BloodType()
+    {
+        cout << "I am warm blooded" << endl;
+    }
+};
 
+class Dog : public Mammal
+{
+public:
+    string name;
+    Dog(string n)
+    {
+        name = n;
+    }
+    void Name()
+    {
+        cout << "  My name is " << name << endl;
+    }
+};
+int main()
+{
+    Dog D1("tONY");
+    D1.BloodType();
+    D1.eat();
+    D1.Name();
 }
