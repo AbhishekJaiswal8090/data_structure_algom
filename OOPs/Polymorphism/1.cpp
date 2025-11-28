3f331966da52a5595d7bb1a6c69633195bafbe69
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-28 10:35:52 +0530

    function overloading

--- OOPs/Polymorphism/1.cpp
+++ OOPs/Polymorphism/1.cpp
@@ -11,8 +11,26 @@ using namespace std;
 
 // Compile time polymorphism
 // 1. Function overloading => we have multiple methods of same name but with different parameter type
-// 2.Opeartor overloading => 
+// 2.Opeartor overloading =>
+
+class Print
+{
+public:
+    void show(int str)
+    {
+        cout << "int :" << str << endl;
+    }
+    void show(string str)
+    {
+        cout << "string :" << str << endl;
+    }
+};
 
 int main()
 {
+    Print p1;
+    p1.show(56);
+    p1.show("abhi");
+
+    return 0;
 }
