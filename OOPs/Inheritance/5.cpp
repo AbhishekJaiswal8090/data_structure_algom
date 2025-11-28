2b8cc98213217e7e3eec5b82493cda5743748ff8
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-28 10:23:29 +0530

    hybrid inheritance

--- OOPs/Inheritance/5.cpp
+++ OOPs/Inheritance/5.cpp
@@ -2,6 +2,43 @@
 using namespace std;
 
 // Now we study Hierarchial inheritance
+// a inheritance in which w have single base class while we have multiple derived class
+
+class Animal
+{
+public:
+    void eat()
+    {
+        cout << "i can eat" << endl;
+    }
+    void Breathe()
+    {
+        cout << "I can brethe" << endl;
+    }
+};
+
+class Bird : public Animal
+{
+public:
+    void Fly()
+    {
+        cout << "I can fly" << endl;
+    }
+};
+
+class Mammal : public Animal
+{
+public:
+    void walk()
+    {
+        cout << "I can walk" << endl;
+    }
+};
+
 int main()
 {
+    Mammal m1;
+    m1.Breathe();
+    m1.eat();
+    m1.walk();
 }
