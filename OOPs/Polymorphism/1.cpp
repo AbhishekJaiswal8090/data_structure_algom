12e7faf5fc09b453c4c39cd98eb8f8078b4878ab
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-28 10:28:46 +0530

    basic of polymorphism

---
+++ OOPs/Polymorphism/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Polymorphism
+// Polymorphism is the ability of objects to take on different forms or behave in different
+// ways depending on context in which they are used
+
+// polymorphism are of two types
+// Compile Time Polymorphism
+// run time polymorphiism
+
+int main()
+{
+}
