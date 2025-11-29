cdea3fbb5b7bf9515c194dfe995e62dfad198e5a
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-29 11:54:42 +0530

    Abstaction in cpp

---
+++ OOPs/Abstraction/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Abstraction
+// Hiding all unncessary detail and showing only the important parts
+
+
+// Abstract class and PUre Virtual functions 
+
+int main()
+{
+}
