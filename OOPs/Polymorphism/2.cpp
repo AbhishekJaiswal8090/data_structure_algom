3b7137b3f201ee31bfd0aa1386250a45a2e39c63
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-29 11:34:55 +0530

    function overriden

--- OOPs/Polymorphism/2.cpp
+++ OOPs/Polymorphism/2.cpp
@@ -1,12 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // runtime polymorphism = >
-// Function overriding 
+// Function overriding
 
 // Parent and child both contain the same function with different implementation
 // THe parent class function is said to be overriden
 
-int main(){
+class Parent
+{
+public:
+    void show()
+    {
+        cout << "parent clas show..";
+    }
+};
 
+class Child : public Parent
+{
+public:
+    void show()
+    {
+        cout << "child class show..";
+    }
+};
+
+int main()
+{
+    Child child1;
+    child1.show();
 }
