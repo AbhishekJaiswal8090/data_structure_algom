bba4687603d2642f0abfe2c78d2947dab2dad960
Abhishek <abhi84514aaa.in@gmail.com>
2025-11-29 11:30:48 +0530

    basics of overriding

--- OOPs/Polymorphism/2.cpp
+++ OOPs/Polymorphism/2.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 
+// runtime polymorphism = >
 // Function overriding 
 
+// Parent and child both contain the same function with different implementation
+// THe parent class function is said to be overriden
 
 int main(){
 
