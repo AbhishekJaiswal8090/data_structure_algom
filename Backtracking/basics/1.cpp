93a17542c8a0c3482273ccd910ce6ada1001072e
Abhishek <abhi84514aaa.in@gmail.com>
2025-12-04 23:40:49 +0530

    backtracking on arrays

--- Backtracking/basics/1.cpp
+++ Backtracking/basics/1.cpp
@@ -1,10 +1,41 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-
 
 // Backtracking
 
+// usually backtracking cosist of three types of problems
+// Decision
+// Opimization
+// Enumeration
 
-int main(){
+void PrintArr(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << ",";
+    }
+    cout << endl;
+}
 
+void changeArr(vector<int> &arr, int i, int n)
+{
+    if (i == n)
+    {
+        PrintArr(arr, n);
+        return;
+    }
+    int old = arr[i];
+    arr[i] = i + 1;
+    changeArr(arr, i + 1, n); // recurse to next index
+    arr[i] = old;             // restore previous value (backtrack)
+}
+
+int main()
+{
+
+    // backtracking on arrays
+    vector<int> arr = {1, 2, 3, 4, 5};
+    changeArr(arr, 0, 5);
+    return 0;
 }
