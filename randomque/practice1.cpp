dd9a716f2ffb057802dc5340dda1138e46c66a8e
Abhishek <abhi84514aaa.in@gmail.com>
2025-12-08 13:42:55 +0530

    problem solving

---
+++ randomque/practice1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+char upper(char c)
+{
+    return 'A' + (c - 'a');
+}
+
+int main()
+{
+    while (true)
+    {
+        string s;
+        cin >> s;
+
+        if (s.size() == 0)
+        {
+            break;
+        }
+        string ans = "";
+        for (int i = 0; i < s.size(); i++)
+        {
+            ans += upper(s[i]);
+        }
+
+        cout << ans << endl;
+    }
+}
