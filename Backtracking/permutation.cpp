9f22840ece9a109f83463a5d0cd6efc76d7d7efc
Abhishek <abhi84514aaa.in@gmail.com>
2025-12-11 13:24:03 +0530

    genarating permuation

---
+++ Backtracking/permutation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// NOw we are going to see the generating permutation using baktracking 
+// find and print all permuation / arrangements of a given string
+// num of permuation is always equals to n! where n is number of elements 
+
+
+int main(){
+
+
+
+}
