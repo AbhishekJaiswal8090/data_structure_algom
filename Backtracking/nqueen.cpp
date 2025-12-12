665cefcbaad1f35b325032aeedcf91078082a49c
Abhishek <abhi84514aaa.in@gmail.com>
2025-12-12 12:31:30 +0530

    n queens problem

--- Backtracking/nqueen.cpp
+++ Backtracking/nqueen.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // n queen problem
 
+void PrintChessBoard(vector<vector<char>> &board, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "...............\n";
+}
+
+void chessBoard(vector<vector<char>> &board, int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        vector<char> newRow;
+        for (int j = 0; j < n; j++)
+        {
+            newRow.push_back('.');
+        }
+        board.push_back(newRow);
+    }
+}
+
+void nQueens(vector<vector<char>> &board, int row)
+{
+    int n = board.size();
+
+    if (row == n)
+    {
+        PrintChessBoard(board, n);
+        return;
+    }
+
+    for (int j = 0; j < n; j++)
+    { // cols
+        board[row][j] = 'Q';
+        nQueens(board, row + 1);
+        board[row][j] = '.';
+    }
+}
+
 int main()
 {
-    
+    vector<vector<char>> board;
+    int n = 2;
+
+    chessBoard(board, n);
+    PrintChessBoard(board, n);
+    nQueens(board, 0);
+    return 0;
 }
