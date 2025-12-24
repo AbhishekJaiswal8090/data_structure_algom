de13ef69fe180380cabb1755d88cbe14c8c9dbdd
Abhishek <abhi84514aaa.in@gmail.com>
2025-12-24 16:37:19 +0530

    Greedy approach

---
+++ greedy/basics/1.cpp
@@ -0,0 +1,10 @@
+// Today we are going to study the greedy approach
+// Mkaing greedy choices at each step
+// chose the local optimum and hope for global optimum
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+}
