4f16c01c858f05c4a6436d3557b74a90f755b55a
Abhishek <abhi84514aaa.in@gmail.com>
2026-02-15 21:19:24 +0530

    queue details

---
+++ STLs/queue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// queue in depth
+// queue is a data structure which follows FIFO pattern
+// FIRST IN FIRST OUT
+
+// it has two parts 
+// front and Rear
+// insertion takes place by front
+// deletion takes place by rear
+
+
+int main()
+{
+}
