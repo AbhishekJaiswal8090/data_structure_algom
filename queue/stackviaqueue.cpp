9b3c6a15e03348929ac2bb5b562249e4a8f9acae
Abhishek <abhi84514aaa.in@gmail.com>
2026-02-19 23:41:06 +0530

    stack using 2 queue implementation

--- queue/stackviaqueue.cpp
+++ queue/stackviaqueue.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
+// #stack using two queues
+
+class Stack
+{
+    queue<int> q1;
+    queue<int> q2;
+
+public:
+    void push(int ele)
+    {
+        while (!q1.empty())
+        {
+            int data = q1.front();
+            q1.pop();
+            q2.push(data);
+        }
+        q1.push(ele);
+        while (!q2.empty())
+        {
+            int data = q2.front();
+            q1.push(data);
+            q2.pop();
+        }
+    }
+    int pop()
+    {
+        int res = q1.front();
+        q1.pop();
+        return res;
+    }
+    int top()
+    {
+        return q1.front();
+    }
+    bool empty()
+    {
+        return q1.empty();
+    }
+};
 int main()
 {
-    
+    Stack s1;
+    s1.push(120);
+    s1.push(30);
 }
