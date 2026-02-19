27d6c84a8a8af8a6191c1f95eaf439042aa589f7
Abhishek <abhi84514aaa.in@gmail.com>
2026-02-20 00:23:18 +0530

    problem solving

--- queue/problem1.cpp
+++ queue/problem1.cpp
@@ -1,6 +1,31 @@
-#include <iostaream>
+#include <iostream>
+#include <queue>
 using namespace std;
+
+void NonRepeating(string s)
+{
+    int freq[26] = {0};
+    queue<pair<char, int>> q;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        freq[s[i] - 'a']++;
+        q.push({s[i], i});
+
+        while (!q.empty() && freq[q.front().first - 'a'] > 1)
+        {
+            q.pop();
+        }
+    }
+
+    if (!q.empty())
+        cout << q.front().first;
+    else
+        cout << "No repeating character";
+}
 
 int main()
 {
+    string s = "pprrw";
+    NonRepeating(s);
 }
