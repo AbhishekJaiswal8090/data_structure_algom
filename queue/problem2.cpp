88f6dbe3dd26e798c5f7cf92766157b85f05c988
Abhishek <abhi84514aaa.in@gmail.com>
2026-02-22 17:27:24 +0530

    optimized solution

--- queue/problem2.cpp
+++ queue/problem2.cpp
@@ -7,14 +7,9 @@ using namespace std;
 // THe question states that we have even size queue and task is to make the such queue that the fisrt half elelemnts
 // and second half elelments shows up alernatively
 
-int main()
+// Brute force
+void INterLeave(queue<int> &q)
 {
-    queue<int> q;
-    q.push(2);
-    q.push(4);
-    q.push(3);
-    q.push(1);
-
     int n = q.size();
     int half = n / 2;
 
@@ -46,6 +41,47 @@ int main()
         cout << myq.front();
         myq.pop();
     }
+}
 
+void optimized(queue<int> &q)
+{
+    int n = q.size();
+    int half = n / 2;
+
+    queue<int> fh, sh;
+
+    for (int i = 0; i < half; i++)
+    {
+        fh.push(q.front());
+        q.pop();
+    }
+
+    while (!fh.empty())
+    {
+        q.push(fh.front());
+        fh.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    while (!q.empty())
+    {
+        cout << myq.front();
+        myq.pop();
+    }
+}
+
+int main()
+{
+    queue<int> q;
+    q.push(2);
+    q.push(4);
+    q.push(3);
+    q.push(1);
+
+    INterLeave(q);
+    // It uses two extra queues
+    optimized(q);
+    // iT ONLY USES SINGLE EXTRA QUEUES
     return 0;
 }
