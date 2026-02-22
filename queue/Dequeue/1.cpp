c01129611f267d651b11aa3a03d1576618b8df0f
Abhishek <abhi84514aaa.in@gmail.com>
2026-02-22 19:03:51 +0530

    deque mehtods

--- queue/Dequeue/1.cpp
+++ queue/Dequeue/1.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include < deque>
 using namespace std;
 
 // IN today class we are going to learn dequeue
 // Deque means double ended queue
-
+// This is as same as queue but in queue we can only push elelement in last
+// but in dequeue we can push elelemnt from both front and back same with pop operation
 
 int main()
 {
+    deque<int> dq;
+    dq.push_back(20);
+    dq.push_back(40);
+    dq.push_front(40);
+    dq.push_front(70);
 
+    dq.pop_back();
+    dq.pop_front();
+
+    return 0;
 }
