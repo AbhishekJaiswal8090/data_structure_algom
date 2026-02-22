5e06491fa6208428750706b0ba78b054f6a4c475
Abhishek <abhi84514aaa.in@gmail.com>
2026-02-22 17:45:34 +0530

    queue reversal done

--- queue/queuereversal.cpp
+++ queue/queuereversal.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <queue>
+#include <stack>
 using namespace std;
 
 // Queue REversal
+
+// we will use s stack in ordrer to reverse an queue
+
+void QueueReversal(queue<int> &q)
+{
+    queue<int> q;
+    q.push(20);
+    q.push(30);
+    q.push(97);
+    q.push(60);
+
+    stack<int> s;
+    while (!q.empty())
+    {
+        s.push(q.front());
+        q.pop();
+    }
+
+    while (!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+}
 
 int main()
 {
