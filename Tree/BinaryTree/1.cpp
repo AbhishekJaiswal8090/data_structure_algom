ff46fcdbc0f5a358e7f3c8656a6cc40bb73564b5
Abhishek <abhi84514aaa.in@gmail.com>
2026-02-23 21:31:18 +0530

    Tree data structure

---
+++ Tree/BinaryTree/1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+// In today lecture we are going to study tree
+// The most complicated data strcuture
+// Which is Hierarchial Data Structure
+
+int main(){
+
+}
