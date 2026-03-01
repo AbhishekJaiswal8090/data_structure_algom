96bcee1fe84aa23f942d2f30ba5db67e8e72fd8b
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-01 10:58:02 +0530

    Travresal done

--- Tree/BinaryTree/3.cpp
+++ Tree/BinaryTree/3.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
-// Today we are going to study BFS
-// Breadth First Search
-// it traverse level by level which is called Level Order Traversal
-
+// Node definition
 class Node
 {
 public:
@@ -19,31 +17,36 @@ public:
     }
 };
 
-Node *BuildNode(int nodes[], int size, int idx)
+// Build tree from vector
+
+Node *BuildNode(vector<int> &nodes, int &idx)
 {
-    if (idx >= size || nodes[idx] == -1)
+    if (nodes[idx] == -1)
     {
+        idx++;
         return NULL;
     }
-
-    Node *currNode = new Node(nodes[idx]);
-    currNode->left = BuildNode(nodes, size, 2 * idx + 1);
-    currNode->right = BuildNode(nodes, size, 2 * idx + 2);
+    Node *currNode = new Node(nodes[idx++]);
+    currNode->left = BuildNode(nodes, idx);
+    currNode->right = BuildNode(nodes, idx);
     return currNode;
 }
 
-// Level Order Traversal
+// Level Order Traversal (BFS)
 void LevelOrderTraversal(Node *root)
 {
     if (root == NULL)
         return;
+
     queue<Node *> q;
     q.push(root);
+
     while (!q.empty())
     {
         Node *current = q.front();
         q.pop();
         cout << current->data << " ";
+
         if (current->left != NULL)
         {
             q.push(current->left);
@@ -54,12 +57,16 @@ void LevelOrderTraversal(Node *root)
         }
     }
 }
+
 int main()
 {
-    int nodes[] = {1, 2, 4, 5, 3, 6, 7};
-    int size = 7;
-    Node *root = BuildNode(nodes, size, 0);
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    int idx = 0;
+    Node *root = BuildNode(nodes, idx);
+
+    cout << "Level Order Traversal: ";
     LevelOrderTraversal(root);
     cout << endl;
+
     return 0;
 }
