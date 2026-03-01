2432402b0c63fdb18a31926112a13d4d03d3dd9b
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-01 11:19:03 +0530

    Height of binarytree

--- Tree/BinaryTree/4.cpp
+++ Tree/BinaryTree/4.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 // Today we are going to study about height of the tree
+// Height is basically the distance bw the root node and the leaf node 
+// So the height of the tree an vary on two component edge and node and if we obsereve a tree 
+// height of edge = height of node - 1;
+// so all we need is to calculate the height
+
 
 int main()
 {
