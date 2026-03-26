eadb616d84f73d3130356263d5eed1a845838a70
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-26 18:14:59 +0530

    Solving LCA Problem

---
+++ Tree/BinaryTree/prt3/2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// LOwest Common Ancestor
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+int main()
+{
+}
