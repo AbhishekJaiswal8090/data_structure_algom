c498872d7a4205cce11d18e7a2a7696bdb4e440a
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-30 23:59:09 +0530

    Solution of LCA

--- Tree/BinaryTree/prt3/2.cpp
+++ Tree/BinaryTree/prt3/2.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // IN today lecture we are going to study and solve the problem Lowest Common Ancestor
 // We will solve it using two aproaches
 // This apprach reuires o(n) Time complexity and o(n) space cmplexity
+
 class Node
 {
 public:
@@ -33,6 +34,58 @@ Node *BuildNode(vector<int> &nodes, int &idx)
     return currNode;
 }
 
+bool calculatePath(Node *root, vector<int> &path, int n)
+{
+    if (root == NULL)
+    {
+        return false;
+        ;
+    }
+    path.push_back(root->data);
+    if (root->data == n)
+    {
+        return true;
+    }
+
+    int left = calculatePath(root->left, path, n);
+    int right = calculatePath(root->right, path, n);
+
+    if (left || right)
+        return true;
+
+    if (!left && !right)
+        return false;
+
+    path.pop_back();
+    return false;
+}
+
+int LCA(Node *root, int n1, int n2)
+{
+
+    vector<int> path1;
+    vector<int> path2;
+
+    calculatePath(root, path1, n1);
+    calculatePath(root, path2, n2);
+
+    int lca = -1;
+    for (int i = 0, j = 0; i < path1.size() && j < path2.size(); i++, j++)
+    {
+        if (path1[i] != path2[j])
+        {
+            return lca;
+        }
+        lca = path1[i];
+    }
+    return lca;
+}
+
 int main()
 {
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    int idx = 0;
+    Node *root = BuildNode(nodes, idx);
+    cout << "LCA IS " << LCA(root, 4, 5) << endl;
+    return 0;
 }
