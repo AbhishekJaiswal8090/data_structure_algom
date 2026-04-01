e34963389fd67554d63dd6c6c34b45e25b68579d
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-01 16:58:49 +0530

    Complete solution LCA 2nd Approach

--- Tree/BinaryTree/prt3/3.cpp
+++ Tree/BinaryTree/prt3/3.cpp
@@ -34,6 +34,44 @@ Node *BuildNode(vector<int> &nodes, int &idx)
 // Beacause if you notice the first approach time complexity is o(n) and while space complexity is also o(n)
 // So in this lecture we are going to find a solution which works on time compleixity o(n) while reducing space complexity to o(1)
 
+Node *Lca(Node *root, int n1, int n2)
+{
+    if (root == nullptr)
+    {
+        return NULL;
+    }
+    if (root->data == n1 || root->data == n2)
+    {
+        return root;
+    }
+
+    Node *leftval = Lca(root->left, n1, n2);
+    Node *rightval = Lca(root->right, n1, n2);
+
+    if (leftval != nullptr && rightval != nullptr)
+    {
+        return root;
+    }
+    if (leftval == nullptr && rightval != nullptr)
+    {
+        return rightval;
+    }
+    if (leftval != nullptr && rightval == nullptr)
+    {
+        return leftval;
+    }
+    if (leftval == nullptr && rightval == nullptr)
+    {
+        return nullptr;
+    }
+}
+
 int main()
 {
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    int idx = 0;
+    Node *root = BuildNode(nodes, idx);
+    Node *val = Lca(root, 4, 3);
+    cout << "LCA IS " << val->data << endl;
+    return 0;
 }
