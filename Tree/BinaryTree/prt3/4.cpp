ce16eab480d485f38de1f929cb81292f259f2612
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-06 23:13:02 +0530

    minimum dist bw two nodes problem

--- Tree/BinaryTree/prt3/4.cpp
+++ Tree/BinaryTree/prt3/4.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+// IN this lecture we are going to study
+// to find out the minimum distance bw two nodes
 class Node
 {
 public:
@@ -27,6 +30,42 @@ Node *BuildNode(vector<int> &nodes, int &idx)
     currNode->right = BuildNode(nodes, idx);
 
     return currNode;
+}
+
+Node *Lca(Node *root, int n1, int n2)
+{
+    if (root == nullptr)
+    {
+        return NULL;
+    }
+    if (root->data == n1 || root->data == n2)
+    {
+        return root;
+    }
+
+    Node *leftval = Lca(root->left, n1, n2);
+    Node *rightval = Lca(root->right, n1, n2);
+
+    if (leftval != nullptr && rightval != nullptr)
+    {
+        return root;
+    }
+    if (leftval == nullptr && rightval != nullptr)
+    {
+        return rightval;
+    }
+    if (leftval != nullptr && rightval == nullptr)
+    {
+        return leftval;
+    }
+    if (leftval == nullptr && rightval == nullptr)
+    {
+        return nullptr;
+    }
+}
+
+int minDist(Node *root, Node *p, Node *q)
+{
 }
 
 int main()
