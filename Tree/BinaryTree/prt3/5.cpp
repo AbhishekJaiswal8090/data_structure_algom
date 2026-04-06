de55460061fb0edc4585f1b7fe1df7089305583f
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-06 23:48:47 +0530

    Appraoch for kth ancestor problem

--- Tree/BinaryTree/prt3/5.cpp
+++ Tree/BinaryTree/prt3/5.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// In this leture we are going to study about the problem
+// finding kth ancestor
+// You can search te problem statemnet
+// SO our approach here is that we will try to find the our node
+// if we find it we'll backtrack counting the nodes level and once this level reach to k we'll print our answer
+// if node doesn't exist we will return null;
+
 class Node
 {
 public:
@@ -27,6 +34,10 @@ Node *BuildNode(vector<int> &nodes, int &idx)
     currNode->right = BuildNode(nodes, idx);
 
     return currNode;
+}
+
+void KthAncestor(Node *root, int node, int k)
+{
 }
 
 int main()
