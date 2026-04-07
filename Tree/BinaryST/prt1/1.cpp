b652bdb61d06b4a588160cd41532e22304ef602e
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-07 21:18:19 +0530

    explanation of Binary Tree

--- Tree/BinaryST/prt1/1.cpp
+++ Tree/BinaryST/prt1/1.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 // Now we are going to study Binary Search Tree
+// Binaru search tree is a hierarchial data structure
+// A Binary Search Tree (BST) is a special type of Binary Tree with an important rule:
+
+// 🔑 Rule:
+
+// For every node:
+
+// Left subtree → values less than the node
+// Right subtree → values greater than the node
+
+// INorder Traversal of a Binary Search tree gives a sorted sequence always
 
 int main()
 {
