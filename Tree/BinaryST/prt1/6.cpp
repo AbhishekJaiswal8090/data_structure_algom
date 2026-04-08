dccea9438b0d079d53ef41b42b044d6fc42b1242
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-08 05:48:30 +0530

    deletion from a BST

---
+++ Tree/BinaryST/prt1/6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Deletion of a node in an BST
+// THere are three cases in deletion of a node
+// 1=>No child leaf node
+// 2 =>Have 1 child
+// 3=>have 2 children
+
+// lets solve first case 
+
+
+int main()
+{
+}
