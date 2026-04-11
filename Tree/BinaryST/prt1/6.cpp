fe84cb68d5cbd4561b41a1a726675306dd940d1c
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-11 12:07:26 +0530

    Print in range Problem

--- Tree/BinaryST/prt1/6.cpp
+++ Tree/BinaryST/prt1/6.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
 using namespace std;
+// So in this lecture we are going to solve
+// Print in range Problem
 
-// Deletion of a node in an BST
-// THere are three cases in deletion of a node
-// 1=>No child (leaf node)
-// 2 =>Have 1 child
-// 3=>have 2 children
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
 
-// lets solve first case 
+Node *Insert(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        Node *curr = new Node(val);
+        return curr;
+    }
+    if (val < root->data)
+    {
+        root->left = Insert(root->left, val);
+    }
+    else
+    {
+        root->right = Insert(root->right, val);
+    }
+    return root;
+}
 
+Node *BuildBST(int arr[], int n)
+{
+    Node *root = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        root = Insert(root, arr[i]);
+    }
+    return root;
+}
 
 int main()
 {
-    
 }
