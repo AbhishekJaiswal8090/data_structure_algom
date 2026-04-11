3eb0f5c1ec4c15e02fc60c0046fd2694494d7c8a
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-11 11:44:20 +0530

    Deletion of a node in BST

--- Tree/BinaryST/prt1/5.cpp
+++ Tree/BinaryST/prt1/5.cpp
@@ -57,6 +57,50 @@ Node *BuildBST(int arr[], int n) // n nodes and insertion take logn hence total 
     return root;
 }
 
+Node *getInorderS(Node *root)
+{
+    while (root->left != NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+Node *DelNode(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (val < root->data)
+    {
+        root->left = DelNode(root->left, val);
+    }
+    else if (val > root->data)
+    {
+        root->right = DelNode(root->right, val);
+    }
+    else
+    {
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+
+        if (root->left == NULL || root->right == NULL)
+        {
+            return (root->left == NULL ? root->right : root->left);
+        }
+
+        Node *IS = getInorderS(root->right);
+        root->data = IS->data;
+        root->right = DelNode(root->right, IS->data);
+        return root;
+    }
+    return root;
+}
+
 int main()
 {
     int arr[6] = {5, 1, 3, 4, 2, 7};
