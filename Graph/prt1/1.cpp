436ed505ce856d0544eebe50b1257c2503a7823f
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-16 00:36:25 +0530

    Graph explanation

--- Graph/prt1/1.cpp
+++ Graph/prt1/1.cpp
@@ -1,15 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-// No starting from this lecture we are going to study 
+// No starting from this lecture we are going to study
 // Graph Data Structure
 
 // Graph is made up of or collection of vertices (nodes) and edges
 
-// Tree is a Hierarchial data structure while graph is a 
-// network of nodes 
+// Tree is a Hierarchial data structure while graph is a
+// network of nodes
 
-int main(){
+// Application of graph are vast
+// Into the filed of networks , routing and shortest path kind of problems
+// THre are multiple applications into the society of graph
 
+// There are diff types of Graph based on edges
+// 1-> UniDirectional(one way)
+// 2-> UnDirected (no way mentioned)
+// 3-> BiDirectional(both have directions)
+
+// on the basis of wieght
+// 1->Weighted
+// 2->Unwighted
+
+// So for the basic knowledge
+
+// 1. Based on Direction
+// Undirected Graph
+// Directed Graph
+
+// Based on Weights
+// Weighted Graph
+// Unweighted Graph
+
+
+int main()
+{
 }
