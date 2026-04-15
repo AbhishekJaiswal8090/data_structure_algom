4ff4706dc118dc3ece73c2cca41cda32838669e3
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-16 00:22:58 +0530

    Graph Data Structure

---
+++ Graph/prt1/1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+// No starting from this lecture we are going to study 
+// Graph Data Structure
+
+// Graph is made up of or collection of vertices (nodes) and edges
+
+// Tree is a Hierarchial data structure while graph is a 
+// network of nodes 
+
+int main(){
+
+}
