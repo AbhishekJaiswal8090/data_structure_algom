6f5ca1c90dbc5d41b010da0f50b8cba6a1b1d69f
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-17 00:18:48 +0530

    graph representation using adjacency list

--- Graph/prt1/2.cpp
+++ Graph/prt1/2.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 // Graph Representation
 
+// 1-> Graphs are represented using Adjacency list and matrix
+// In this lecture we are going to represent graph using Adjacency list
+
 int main()
 {
 }
