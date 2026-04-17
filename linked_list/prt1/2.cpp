3dc11e330116401e6509e354eb13683c7fca811c
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-17 22:42:19 +0530

    linked list functions implementation

--- linked_list/prt1/2.cpp
+++ linked_list/prt1/2.cpp
@@ -4,12 +4,20 @@ using namespace std;
 // Linked list implementation
 
 // so here Node class represents the the entire node which contains the data and the pointer pointting to the next elements
+// and the other class List represents the linked list itself which contains the info of head and tail
+
+//  and now we are going to implement methods and functions of linked list
+// push_fornt
+// push-back
+// pop front
+// push back
+
 class Node
 {
+public:
     int data;
     Node *next;
 
-public:
     Node(int data)
     {
         this->data = data;
@@ -28,10 +36,27 @@ public:
         Head = NULL;
         Tail = NULL;
     }
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (Head == NULL)
+        {
+            Head = newNode;
+        }
+        else
+        {
+            newNode->next = Head;
+            Head = newNode;
+        }
+    }
 };
 
 int main()
 {
     List ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
     return 0;
 }
