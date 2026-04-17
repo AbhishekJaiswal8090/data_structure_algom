d899cdc367bed46da58fb558a05587b2c48c4d2b
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-17 21:44:34 +0530

    Linked list implmnetation in detail

--- linked_list/prt1/2.cpp
+++ linked_list/prt1/2.cpp
@@ -3,6 +3,35 @@ using namespace std;
 
 // Linked list implementation
 
+// so here Node class represents the the entire node which contains the data and the pointer pointting to the next elements
+class Node
+{
+    int data;
+    Node *next;
+
+public:
+    Node(int data)
+    {
+        this->data = data;
+        next = nullptr;
+    }
+};
+
+class List
+{
+    Node *Head;
+    Node *Tail;
+
+public:
+    List()
+    {
+        Head = NULL;
+        Tail = NULL;
+    }
+};
+
 int main()
 {
+    List ll;
+    return 0;
 }
