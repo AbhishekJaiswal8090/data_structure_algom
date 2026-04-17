90374a65324220016bb62d233a9428cdfb1c6345
Abhishek <abhi84514aaa.in@gmail.com>
2026-04-17 21:28:26 +0530

    arrays cs linked list

--- linked_list/prt1/1.cpp
+++ linked_list/prt1/1.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-// From now on we are going to study linked list 
+// From now on we are going to study linked list
 // The linked list is a linear data structure made up of nodes
 // where each node contains data and pointer to the next node
 // compare to arrays they are more flexible in insertion and deletion
-// but traversal and accessing elements takes time unlike arrays you can just access by indexes 
+// but traversal and accessing elements takes time unlike arrays you can just access by indexes
 
+// Node => (data and pointer)
+// moreover the arrays are contigous but linked list aren't they are connected my pointers
+// the nodes could be anywhere in memory but connected by pointer
 
-int main(){
-
+int main()
+{
 }
